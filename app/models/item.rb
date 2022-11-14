ca45c365b759c566068ab46adcda3349c092095b
kotsaito <intel.55.kota@gmail.com>
2022-11-15 00:39:18 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,12 @@ class Item < ApplicationRecord
   belongs_to :user
   #has_one :purchase_history
 
-
+  validates :product_name,  presence: true
+  validates :description,   presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,   presence: true
 end
