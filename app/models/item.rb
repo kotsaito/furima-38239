3a9b6209c16fa17635eef581f57e83572af20745
kotsaito <intel.55.kota@gmail.com>
2022-11-14 13:23:28 +0900

    activ hash 記述

---
+++ app/models/item.rb
@@ -0,0 +1,12 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  belongs_to :user
+  #has_one :purchase_history
+
+
+end
