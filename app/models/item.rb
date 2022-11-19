df2cf0035d8a356272cbcf866fddf2bff79da2cc
kotsaito <intel.55.kota@gmail.com>
2022-11-19 20:13:22 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,7 @@ class Item < ApplicationRecord
   #has_one :purchase_history
   has_one_attached :image
 
+  validates :image,  presence: true
   validates :product_name,  presence: true
   validates :description,   presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
@@ -16,8 +17,9 @@ class Item < ApplicationRecord
   validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
-  with_options presence: true, format: { with: /\A[0-9]+|z/, message: 'is invalid. Input half-width characters' } do
-   validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999,message: "is out of setting range"}
+  validates :price, presence: true, 
+            inclusion: { in: 300..9_999_999, message: "is out of setting range" }, 
+            format: { with: /\A[0-9]+\z/ },
+            numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
 
-  end
 end
