3f8a18462af67ef0bbfb17c43e64dca762126aee
kotsaito <intel.55.kota@gmail.com>
2022-12-14 01:17:24 +0900

    formオブジェクト実装

---
+++ app/controllers/purchase_histories_controller.rb
@@ -0,0 +1,34 @@
+class PurchaseHistoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :redirect_root, only: :index
+
+
+  def index
+    @purchase_history_order = PurchaseHistoryOrder.new
+  end
+
+  def create
+    @purchase_history_order = PurchaseHistoryOrder.new( purchase_history_params)
+    if @purchase_history_order.valid?
+      @purchase_history_order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_history_params
+    params.require(:purchase_history_order).permit(:post_code, :prefecture_id, :city, :address, :building_name, :tell).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def redirect_root
+    redirect_to root_path if current_user.id == @item.user_id || @item.purchase_history
+  end
+end
---
+++ app/models/purchase_history_order.rb
@@ -0,0 +1,19 @@
+class PurchaseHistoryOrder
+  include ActiveModel::Model
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :tell, :item_id, :user_id
+
+  validates :post_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :tell, presence: true, 
+            length: { minimum: 10, maximum: 11},
+            numericality: { only_integer: true, message: "is invalid. Input only number" }
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+
+  def save
+    purchase_history = PurchaseHistory.create(item_id: item_id, user_id: user_id)
+    Order.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, tell: tell, purchase_history_id: purchase_history.id)
+  end
+end
